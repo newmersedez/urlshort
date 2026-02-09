0f03035b90c2906fb3b82773c880f9d5be82da4d
newmersedez <volkovdm333@gmail.com>
2026-02-09 21:04:41 +0300

    fixed default created_at value in migration.

--- migrations/000002_create_shorten_urls.up.sql
+++ migrations/000002_create_shorten_urls.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE shorten_urls (
     id              VARCHAR(8) PRIMARY KEY,
     original_value  VARCHAR(2048) NOT NULL,
-    created_at      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+    created_at      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT (now() AT TIME ZONE 'UTC')
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS ix_shorten_urls_original_value ON shorten_urls(original_value);
