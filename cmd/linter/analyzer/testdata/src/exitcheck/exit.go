525ea87f49e569efabdb8e980e10902994899c43
newmersedez <volkovdm333@gmail.com>
2026-05-13 23:36:00 +0300

    fixed tests

---
+++ cmd/linter/analyzer/testdata/src/exitcheck/exit.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func helper() {
+	os.Exit(1)        // want `os\.Exit must not be called outside of main function in main package`
+	log.Fatal("err")  // want `log\.Fatal must not be called outside of main function in main package`
+}
+
+func main() {
+	os.Exit(0)
+	log.Fatal("error")
+}
