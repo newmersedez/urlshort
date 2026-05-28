05a129f6beecae6f6aa42711f9f5953622ca3f7d
newmersedez <volkovdm333@gmail.com>
2026-05-28 22:36:26 +0300

    fixed formatting

--- cmd/linter/analyzer/testdata/src/exitcheck/exit.go
+++ cmd/linter/analyzer/testdata/src/exitcheck/exit.go
@@ -6,10 +6,10 @@ import (
 )
 
 func helper() {
-	os.Exit(1)         // want `os\.Exit must not be called outside of main function in main package`
-	log.Fatal("err")   // want `log\.Fatal must not be called outside of main function in main package`
-	log.Fatalf("%s", "err")  // want `log\.Fatalf must not be called outside of main function in main package`
-	log.Fatalln("err") // want `log\.Fatalln must not be called outside of main function in main package`
+	os.Exit(1)              // want `os\.Exit must not be called outside of main function in main package`
+	log.Fatal("err")        // want `log\.Fatal must not be called outside of main function in main package`
+	log.Fatalf("%s", "err") // want `log\.Fatalf must not be called outside of main function in main package`
+	log.Fatalln("err")      // want `log\.Fatalln must not be called outside of main function in main package`
 }
 
 func main() {
